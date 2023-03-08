5ff06dfb25f27003642948777f0e317ddf2c9206
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-03-07 21:29:44 -0300

    Write the code

--- App.java
+++ App.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args){
 
+        int numberToMultiplicationTable;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Qual tabuada deseja visualizar?");
+        numberToMultiplicationTable = input.nextInt();
+
+        for (int i = 1; i <= 10; i++){
+
+            System.out.println(numberToMultiplicationTable + " x " + i + " = " + numberToMultiplicationTable * i);
+
+        }
+        
+        input.close();
 
     }
 
